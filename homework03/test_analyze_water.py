dee31f0d87cb8da2c9917a7eb819dcd2738cc14f
Carel Soney <carelsoney@gmail.com>
2023-02-12 23:36:18 -0600

    please just work

--- homework03/test_analyze_water.py
+++ homework03/test_analyze_water.py
@@ -1,9 +1,11 @@
 from analyze_water import calc_turbidity, safe_time
 # importing the two functions created in the analyze_water file
+import pytest
+# importing library needed to do unit testing
 
 def test_calc_turbidity(): # testing first function to verify that the result is the product of the two numbers
-   assert calc_turbidity(1.0, 2.0) == 1.0 * 2.0
-   assert calc_turbidity(8.5, 2.3) == 8.5 * 2.3
+   assert calc_turbidity(1.0, 2.0) == 2.0
+   assert calc_turbidity(8.5, 2.3) == 19.55 # 19.55 = 8.5 * 2.3
 
 def test_safe_time(): #testing second function to verify that minimum time required is accurate
    assert safe_time(1.0) == 0 # number I came up with
