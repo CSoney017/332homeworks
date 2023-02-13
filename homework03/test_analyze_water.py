a8f823b98e985704f9fa9a83a6371bd4b0cabe3e
Carel Soney <carelsoney@gmail.com>
2023-02-12 23:18:30 -0600

    changed names

---
+++ homework03/test_analyze_water.py
@@ -0,0 +1,10 @@
+from analyze_water import calc_turbidity, safe_time
+# importing the two functions created in the analyze_water file
+
+def test_calc_turbidity(): # testing first function to verify that the result is the product of the two numbers
+   assert calc_turbidity(1.0, 2.0) == 1.0 * 2.0
+   assert calc_turbidity(8.5, 2.3) == 8.5 * 2.3
+
+def test_safe_time(): #testing second function to verify that minimum time required is accurate
+   assert safe_time(1.0) == 0 # number I came up with
+   assert safe_time(1.1992) == 8.99 # number specified in the doc
