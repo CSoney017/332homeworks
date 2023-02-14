f1a84764d6b90a2bea6bb936f531e206525f5727
Carel Soney <carelsoney@gmail.com>
2023-02-13 21:15:41 -0600

    finished

--- homework03/analyze_water.py
+++ homework03/analyze_water.py
@@ -1,34 +1,53 @@
-#this is a test comment
-def calc_turbidity(turb_data: list) -> float:
- """
- Will calculate the turbidity of the water sample based on the five most data points in the turb_data list
+#!/usr/bin/env python3
 
- Args:
+def calc_turbidity(recent_data: list) -> float:
+   """
+   Will calculate the turbidity of the water sample based on the five most data points in the turb_data list
+
+   Args:
    turb_data (list): list containing water sample quality data
 
- Returns:
+   Returns:
    turb_sum (float): float that provides the current water turbidity
 
- """
+   """
    turb_sum = 0.0
-   for i in range(len(turb_data)):
-     print(len(turb_data))
-     #cur_a0 = turb_data["calibration_constant"]
-     #cur_I90 = turb_data['detector_current']
-     #turb_sum = turb_sum + cur_a0 * cur_I90
+   for i in range(5): # calculating the turbidity of the five most recent water samples
+     cur_a0 = recent_data[i]["calibration_constant"]
+     cur_I90 = recent_data[i]['detector_current']
+     turb_sum = turb_sum + cur_a0 * cur_I90
 
-   turb_sum = turb_sum / 5
+   turb_sum = turb_sum / 5 # calculating average
+   print("Average turbidity based on the most recent five measurements = ", turb_sum)
    return turb_sum
 
-def safe_time(turb_standard: float, decay_factor: float, avg_turb: float): # what return ???
-   if avg_turb > turb_standard:
+def safe_time(turb_standard: float, decay_factor: float, avg_turb: float) -> float:
+   """
+   Calculates the minimum time needed for water sample to be safe for use
+
+   Args:
+     turb_standard (float): threshold for safe water
+     decay_factor (float): rate at which it decays, expressed as a decimal
+     avg_turb (float) =  current turbidity calculated from function 'calc_turbidity'
+
+   Returns:
+     time_left (float) = minimum time needed for water sample to be safe for use
+   """
+    if avg_turb > turb_standard:
+
+    time_left = 0
+
+    while(avg_turb > turb_standard):
+     time_left = time_left + 1
      print("Warning: Turbidity is above threshold for safe use.")
-     time_safe = math.log(turb_standard/avg_turb, 1 - decay_factor)
-     print("Minimum time to return below a safe threshod = ")
+     time_safe = turb_standard*(1-decay_factor)**time_left
+     print("Minimum time to return below a safe threshod = ", time_safe)
+     break
 
    else:
      print("Info: Turbidity is below threshold for safe use")
      print("Minimum time required to return below a safe threshold = 0 hours")
+   return time_safe
 
 def main():
 
@@ -40,8 +59,10 @@ def main():
 
    response = requests.get(url='https://raw.githubusercontent.com/wjallen/turbidity/main/turbidity_data.json')
    turb_data = response.json()
+   recent_data = turb_data["turbidity_data"][-5:]
 
-   calc_turb = calc_turbidity(turb_data)
+   avg_turb = calc_turbidity(recent_data)
+   part_3 = safe_time(turb_standard, decay_factor, avg_turb)
 
 if __name__ == '__main__':
    main()
