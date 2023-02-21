e190960efef7d2b9a7da7e969d315bb16bdaa368
Ubuntu <ubuntu@c-sy017-vm.js2local>
2023-02-21 04:00:19 +0000

    creating directory

---
+++ homework04/iss_tracker.py
@@ -0,0 +1,58 @@
+from flask import Flask
+
+import requests
+import xmltodict
+import math
+
+app = Flask(__name__)
+
+def get_data() -> dict:
+   response = requests.get(url = 'https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml') #paste link here
+   data = xmltodict.parse(response.text)
+   return data
+
+@app.route('/', methods = ['GET'])
+def total_data() -> dict:
+   iss_data = get_data()
+   return iss_data
+
+@app.route('/epochs', methods = ['GET'])
+def epochs_only() -> dict:
+   iss_data = get_data()
+#   epochs = [] # initializing list to store epochs
+   #accessing only the stateVector data
+
+   data = iss_data['ndm']['oem']['body']['segment']['data']['stateVector']
+
+#   for ii in iss_data
+#     epochs.append(ii['EPOCH'])
+   return data
+
+@app.route('/epochs/<int:epoch>', methods = ['GET'])
+def stateVectors(epoch: str) -> dict:
+   iss_data = epochs_only()
+
+   output = {} # curly brackets = intializing dictionary
+
+   for ii in iss_data:
+     if ii['EPOCH'] == epoch: # searching for specific EPOCH
+       output = ii # entering epoch into dict
+
+       for jj in output:
+
+         if key != 'EPOCH': #inserting value into dict
+           output[key] = float(output[key]['#text'])
+
+   return output
+
+@app.route('/epochs/<epoch>/speed', methods = ['GET'])
+def calc_speed(epoch) -> int:
+   data = stateVectors(epoch)
+   if len(data > 0):
+     speed = math.sqrt(data['X_DOT']**2 + data['Y_DOT']**2 + data['Z_DOT']**2)
+     return str(speed)
+   else:
+     return "Error: data must initialized"
+
+if __name__ == '__main__':
+   app.run(debug = True, host = '0.0.0.0')
