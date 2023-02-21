4523a3809e7ae2544324ef0247f09775595063ef
Ubuntu <ubuntu@c-sy017-vm.js2local>
2023-02-21 05:59:07 +0000

    docstrings

--- homework04/iss_tracker.py
+++ homework04/iss_tracker.py
@@ -7,17 +7,47 @@ import math
 app = Flask(__name__)
 
 def get_data() -> dict:
+  """
+  Enters data from XML file into dictionary
+
+  Args:
+     none
+
+  Returns:
+     iss_data(dict): data in XML file converted into a python dictionary
+  """
+
    response = requests.get(url = 'https://nasa-public-data.s3.amazonaws.com/iss-coords/current/ISS_OEM/ISS.OEM_J2K_EPH.xml') #paste link here
    data = xmltodict.parse(response.text)
    return data
 
 @app.route('/', methods = ['GET'])
 def total_data() -> dict:
+   """
+   Returns data in XML file as a python dictionary
+
+   Args:
+     none
+
+   Returns:
+      iss_data(dict): data in XML file converted into a python dictionary
+   """
+
    iss_data = get_data()
    return iss_data
 
 @app.route('/epochs', methods = ['GET'])
-def epochs_only() -> dict:
+def epochs_only() -> list:
+ """
+ Returns a list of all the epoch values in
+
+ Args:
+  none
+
+ Return:
+  epochs(list): a list of all defined epoch values
+ """
+
    iss_data = get_data()
    epochs = [] # initializing list to store epochs
    data = []
@@ -32,6 +62,16 @@ def epochs_only() -> dict:
 @app.route('/epochs/<epoch>', methods = ['GET'])
 # the less than/greater than sign means that it's a specific value we're hunting for
 def stateVectors(epoch: str) -> dict:
+ """
+  prints out block of data detailing position and velocity for a specific epoch
+
+  args:
+    epoch(str): a specific time within the data
+
+  returns:
+   a dictionary with data containting position (x,y,z) and velocity (x_dot, y_dot, and z_dot) at a specific time (epoc)
+ """
+
    iss_data = get_data()
    iss_data = iss_data['ndm']['oem']['body']['segment']['data']['stateVector']
    output = {} # curly brackets = intializing dictionary
@@ -48,7 +88,16 @@ def stateVectors(epoch: str) -> dict:
    return output
 
 @app.route('/epochs/<epoch>/speed', methods = ['GET'])
-def calc_speed(epoch) -> int:
+def calc_speed(epoch) -> str:
+ """
+  Returns the speed at a specific time
+
+  Args:
+   epoch(str): a specific time
+
+  Returns:
+   speed(str): an int typecasted to a string that calculates the speed at a specific point in time (epoch)
+ """
    data = {}
    data = stateVectors(epoch) # calling function stateVectors
 
